8a59cc83a8ae954006a8c874366ffe4acd1884cd
gamalsobih <gamalsob@gmail.com>
2024-10-18 04:50:28 +0300

    this is my first upload for my portfolio project 80% of function working correctly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './components/home';
+import UserProfile from './components/Userprofile';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import Hero from './components/Hero'
+import { useDispatch } from 'react-redux';
+import { setUserData } from './store/slices/authSlice'; // تأكد من وجود هذا الـ slice
+import { useEffect } from 'react';
+const App = () => {
+  const dispatch = useDispatch();
 
-function App() {
+  useEffect(() => {
+      // محاولة جلب بيانات المستخدم من Local Storage
+      const storedUserData = localStorage.getItem('userData');
+      if (storedUserData) {
+          dispatch(setUserData(JSON.parse(storedUserData))); // تحديث حالة Redux
+      }
+  }, [dispatch]);
+
+  // تعريف التوجيه باستخدام createBrowserRouter
+  const routing = createBrowserRouter([
+      {
+          path: "/",
+          element: <Layout />, // تأكد من وجود مكون Layout
+          children: [
+              { index: true, element: <Home /> },
+              { path: "products", element: <Hero /> },
+              { path: "products/:rat", element: <Hero /> },
+              { path: "login", element: <Login /> },
+              { path: "user-profile", element: <UserProfile /> },
+              { path: "mycart", element: <Cart /> }
+          ]
+      }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={routing} />
   );
-}
+};
 
 export default App;
